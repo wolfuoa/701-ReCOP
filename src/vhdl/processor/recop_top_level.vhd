94ac477635f91f0ef36b68b843c75d0ba5db07f1
Nicholas Wolf <100742628+wolfuoa@users.noreply.github.com>
2024-05-03 14:06:55 +1200

    Deleted instruction fetch

--- src/vhdl/processor/recop_top_level.vhd
+++ src/vhdl/processor/recop_top_level.vhd
@@ -1,51 +1,56 @@
 
 
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_unsigned.ALL;
-USE ieee.std_logic_arith.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
 
-USE work.recop_types.ALL;
-USE work.opcodes.ALL;
-USE work.various_constants.ALL;
+use work.recop_types.all;
+use work.opcodes.all;
+use work.various_constants.all;
 
-ENTITY recop_top_level IS
-    PORT (
-        clk   : IN  bit_1;
-        reset : IN  bit_1;
+entity recop_top_level is
+    port (
+        clk   : in  bit_1;
+        reset : in  bit_1;
 
         -- External Enable Signal
-        ER    : IN  STD_LOGIC;
+        ER    : in  std_logic;
 
         -- External Signal I/O
-        SOP   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-        SIP   : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+        SOP   : out std_logic_vector(15 downto 0);
+        SIP   : in  std_logic_vector(15 downto 0);
 
         -- Communication with NOC
-        DPCR  : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        DPR   : IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
+        DPCR  : out std_logic_vector(31 downto 0);
+        DPR   : in  std_logic_vector(31 downto 0);
 
         -- Signal Transmission Done
-        EOT   : OUT STD_LOGIC;
+        EOT   : out std_logic;
 
     );
-END recop_top_level;
+end recop_top_level;
 
-ARCHITECTURE bhv OF recop_top_level IS
+architecture bhv of recop_top_level is
 
-    SIGNAL PC                  : STD_LOGIC_VECTOR(14 DOWNTO 0);
-    SIGNAL SR                  : STD_LOGIC;
-    SIGNAL instruction         : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    signal PC                  : std_logic_vector(14 downto 0);
+    signal SR                  : std_logic;
+    signal instruction         : std_logic_vector(15 downto 0);
+    signal reset               : std_logic;
+    signal wren                : std_logic := '1';
 
-    SIGNAL instruction_fetched : STD_LOGIC;
+    -- Control unit later
+    signal PC_input_select     : std_logic;
 
-    COMPONENT prog_mem IS
-        PORT (
-            address : IN  STD_LOGIC_VECTOR (14 DOWNTO 0);
-            clock   : IN  STD_LOGIC := '1';
-            q       : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
-        );
-    END COMPONENT;
+    signal instruction_fetched : std_logic;
+
+    -- component prog_mem is
+    --     port (
+    --         address : in  std_logic_vector (14 downto 0);
+    --         clock   : in  std_logic := '1';
+    --         q       : out std_logic_vector (15 downto 0)
+    --     );
+    -- end component;
 
     -- COMPONENT recop_data_path IS
     --     PORT MAP(
@@ -59,34 +64,44 @@ ARCHITECTURE bhv OF recop_top_level IS
     --     );
     -- END COMPONENT;
 
-    COMPONENT second_cycle IS
-        PORT MAP(
-            clk => clk,
-            reset  : IN  STD_LOGIC;
-            enable : IN  STD_LOGIC;
-            q      : OUT STD_LOGIC;
-        );
-    END COMPONENT;
-BEGIN
+    -- COMPONENT second_cycle IS
+    --     PORT MAP(
+    --         clk => clk,
+    --         reset  : IN  STD_LOGIC;
+    --         enable : IN  STD_LOGIC;
+    --         q      : OUT STD_LOGIC;
+    --     );
+    -- END COMPONENT;
+begin
 
-    instruction_memory : prog_mem PORT MAP(
+    instruction_memory : entity work.prog_mem port map(
         address => PC,
         clock   => clk,
         q       => instruction
-    );
+        );
 
-    cycle_counter : second_cycle PORT MAP(
-        clk    => clk,
-        reset  => reset,
-        enable => "1",
-        q      => instruction_fetched
-    );
+    program_counter : entity work.pc port map(
+        clock           => clk,
+        reset           => reset,
+        wren            => wren,
+        pc_input_select => PC_input_select,
 
-    PROCESS
-    BEGIN
-        IF (rising_edge(clk)) THEN
+        jmp_address = >,
+        pc => PC
+        );
+
+    -- cycle_counter : second_cycle port map(
+    --     clk    => clk,
+    --     reset  => reset,
+    --     enable => "1",
+    --     q      => instruction_fetched
+    -- );
+
+    process
+    begin
+        if (rising_edge(clk)) then
             PC <= PC + 1;
-        END IF;
-    END PROCESS;
+        end if;
+    end process;
 
-END bhv;
+end bhv;
