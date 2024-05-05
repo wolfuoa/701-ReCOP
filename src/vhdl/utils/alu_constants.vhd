5b46bd406eef20b4e3e2f8a4cdf00e45e55d9ae9
bcho892 <AKLbc70@gmail.com>
2024-05-05 12:53:51 +1200

    add testbench for alu

--- src/vhdl/utils/alu_constants.vhd
+++ src/vhdl/utils/alu_constants.vhd
@@ -1,14 +1,12 @@
 -- Zoran Salcic
 library ieee;
-use ieee.std_logic_1164.all;
-use work.recop_types.all;
+  use ieee.std_logic_1164.all;
+  use work.recop_types.all;
 
 package alu_constants is
-	-- * ALU operation select defined as alu_op
-	constant alu_add : bit_3 := "000";
-	constant alu_sub : bit_3 := "001";
-	constant alu_and : bit_3 := "010";
-	constant alu_or : bit_3 := "011";
-	constant alu_idle : bit_3 := "100";
-	constant alu_max : bit_3 := "101";
-end alu_constants;
+  -- * ALU operation select defined as alu_op
+  constant alu_add : std_logic_vector(1 downto 0) := "00";
+  constant alu_sub : std_logic_vector(1 downto 0) := "01";
+  constant alu_and : std_logic_vector(1 downto 0) := "10";
+  constant alu_or  : std_logic_vector(1 downto 0) := "11";
+end package;
